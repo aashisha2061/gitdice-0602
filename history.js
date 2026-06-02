08fe86a475435b6bcb51e87738318e0e7dcdf7a1
aashisha2061 <vfa01-wi260047@sankogakuen.jp>
2026-06-02 14:38:30 +0900

    8回目の結果

--- history.js
+++ history.js
@@ -3,6 +3,10 @@ const gameHistory = [
     // ここから下に追記していく
     { turn: 1, player: "playerA", finalPos: "藤沢", event: "無事に移動した！", foundTreasure: false },
     { turn: 2, player: "playerA", finalPos: "相模湖", event: "無事に移動した！", foundTreasure: false },
-    { turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
+    { turn: 3, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
      { turn: 4, player: "playerA", finalPos: "箱根湯本", event: "無事に移動した！", foundTreasure: false },
+     { turn: 5, player: "playerA", finalPos: "小田原", event: "💡 ヒント発見！ 古文書：「東の海を目指す手前、北へと進路を変える分岐点にそれはある」", foundTreasure: false },
+     { turn: 6, player: "playerA", finalPos: "厚木", event: "無事に移動した！", foundTreasure: false },
+     { turn: 7, player: "playerA", finalPos: "相模湖", event: "無事に移動した！", foundTreasure: false },
+     { turn: 8, player: "playerA", finalPos: "横浜", event: "無事に移動した！", foundTreasure: false }
 ];
