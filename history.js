af9eb123000c8ac99a14276b91b36bb7ae758d31
aashisha2061 <vfa01-wi260047@sankogakuen.jp>
2026-06-02 13:55:20 +0900

    1個目のお宝みっけ

--- history.js
+++ history.js
@@ -1,5 +1,8 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-
+    { turn: 1, player: "playerA", finalPos: "藤沢", event: "無事に移動した！", foundTreasure: false },
+    { turn: 2, player: "playerA", finalPos: "相模湖", event: "無事に移動した！", foundTreasure: false },
+    { turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
+     { turn: 4, player: "playerA", finalPos: "箱根湯本", event: "無事に移動した！", foundTreasure: false },
 ];
